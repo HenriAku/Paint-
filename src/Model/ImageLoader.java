d8b25da3b3ecc506b3d7cd9e4dee52b1fc287330
bileluwu <mbchriki@gmail.com>
2025-12-15 09:54:09 +0100

    imageloader

--- src/Model/ImageLoader.java
+++ src/Model/ImageLoader.java
@@ -1,5 +1,17 @@
 package Model;
+import java.awt.*;
 
-public class ImageLoader {
-	
+public class ImageLoader 
+{
+	private Image      image;
+
+	public ImageLoader(String chemin)
+	{
+		this.image = Toolkit.getDefaultToolkit().getImage(chemin);	
+	}
+
+	public Image getImage()
+	{
+		return image;
+	}
 }
