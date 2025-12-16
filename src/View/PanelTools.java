a978b06dc7d9e351c9956dd27293cc0621b5d515
RafaelD135 <debeinrafael@gmail.com>
2025-12-16 18:50:15 +0100

    Commentaire et identation

--- src/View/PanelTools.java
+++ src/View/PanelTools.java
@@ -5,28 +5,34 @@ import javax.swing.JPanel;
 
 import Main.Controller;
 
+/**
+ * PanelTools est le panneau latéral de l'application Paint qui regroupe les outils de dessin et les paramètres associés.
+ */
 public class PanelTools extends JPanel
 {
-	// Attributs
-	private Controller controller;
-	private PanelPalette panelPalette;
+	private Controller     controller     ;
+	private PanelPalette   panelPalette   ;
 	private PanelParametre panelParametres;
 	
-	public PanelTools(Controller ctrl)
+	public PanelTools( Controller ctrl )
 	{
 		this.controller = ctrl;
 
-		this.panelPalette    = new PanelPalette(this.controller);
-		this.panelParametres = new PanelParametre(this.controller);
+		this.panelPalette    = new PanelPalette  ( this.controller );
+		this.panelParametres = new PanelParametre( this.controller );
 
-		this.setLayout(new BorderLayout());
-		this.add(this.panelPalette, BorderLayout.NORTH);
-		this.add(this.panelParametres, BorderLayout.CENTER);
+		this.setLayout( new BorderLayout() );
+
+		this.add( this.panelPalette, BorderLayout.NORTH     );
+		this.add( this.panelParametres, BorderLayout.CENTER );
 	}
 
-	public void showCard(String toolName)
+	/**
+	 * Affiche le panneau de paramètres correspondant à l'outil sélectionné.
+	 * @param toolName Le nom de l'outil dont les paramètres doivent être affichés.
+	 */
+	public void showCard( String toolName )
 	{
-		// Délègue la demande de changement de vue à l'objet PanelParametre
-		this.panelParametres.showCard(toolName);
+		this.panelParametres.showCard( toolName );
 	}
 }
