37f6f7f52a5cefb86d10e331b014483889b7a9e2
RafaelD135 <debeinrafael@gmail.com>
2025-12-18 11:18:36 +0100

    MAJ Indentation et commentaire BucketTool

--- src/Model/BucketTool.java
+++ src/Model/BucketTool.java
@@ -12,7 +12,7 @@ public class BucketTool
 	/**
 	 * Constructeur par défaut
 	 */
-	public BucketTool()	{}
+	public BucketTool() {}
 	
 	/**
 	 * Calcule la distance entre deux couleurs exprimées en entiers
@@ -20,17 +20,17 @@ public class BucketTool
 	 * @param coul2 Deuxième couleur
 	 * @return Distance entre les deux couleurs
 	 */
-	private static double distance(int coul1, int coul2)
+	private static double distance( int coul1, int coul2 )
 	{
-		int r1 = (coul1 >> 16) & 0xFF;
-		int g1 = (coul1 >> 8 ) & 0xFF;
-		int b1 =  coul1        & 0xFF;
+		int r1 = ( coul1 >> 16 ) & 0xFF;
+		int g1 = ( coul1 >> 8  ) & 0xFF;
+		int b1 =   coul1         & 0xFF;
 
-		int r2 = (coul2 >> 16) & 0xFF;
-		int g2 = (coul2 >> 8 ) & 0xFF;
-		int b2 =  coul2        & 0xFF;
+		int r2 = ( coul2 >> 16 ) & 0xFF;
+		int g2 = ( coul2 >> 8  ) & 0xFF;
+		int b2 =   coul2         & 0xFF;
 
-		return Math.sqrt( Math.pow(r1 - r2, 2) + Math.pow(g1 - g2, 2) + Math.pow(b1 - b2, 2) );
+		return Math.sqrt( Math.pow( r1 - r2, 2 ) + Math.pow( g1 - g2, 2 ) + Math.pow( b1 - b2, 2 ) );
 	}
 
 	/**
@@ -48,14 +48,14 @@ public class BucketTool
 
 		colorOrig = imageTarget.getRGB( x, y ) & 0xFFFFFF;
 
-		file.add( new Point(x,y) );
+		file.add( new Point( x, y ) );
 
 		while( !file.isEmpty() )
 		{
 			Point p = file.remove();
 
 			if ( p.x() >= 0 && p.x() < imageTarget.getWidth() && p.y() >= 0 && p.y() < imageTarget.getHeight() &&
-				 distance(colorOrig, imageTarget.getRGB(p.x(), p.y())) < tolerance ) {
+				 distance( colorOrig, imageTarget.getRGB( p.x(), p.y() ) ) < tolerance ) {
 				imageTarget.setRGB( p.x(), p.y(), newColorRGB );
 
 				file.add( new Point( p.x() + 1, p.y() ) );
